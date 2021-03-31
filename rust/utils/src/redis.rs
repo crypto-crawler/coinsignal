52f05718ddcab0a70fb923e0cb7b6800ac0e8eda
soulmachine <soulmachine@gmail.com>
2021-03-30 18:52:12 -0700

    Logged error

--- rust/utils/src/redis.rs
+++ rust/utils/src/redis.rs
@@ -2,14 +2,20 @@ use log::*;
 use std::time::Duration;
 
 pub fn wait_redis(redis_url: &str) {
-    let client = redis::Client::open(redis_url).unwrap();
-    let mut conn = client.get_connection().unwrap();
     loop {
-        let resp = redis::cmd("PING").query::<String>(&mut conn);
-        if let Ok(pong) = resp {
-            if pong == "PONG" {
-                break;
+        let client = redis::Client::open(redis_url).unwrap();
+        match client.get_connection() {
+          Ok(mut conn) => {
+            let resp = redis::cmd("PING").query::<String>(&mut conn);
+            if let Ok(pong) = resp {
+                if pong == "PONG" {
+                    break;
+                }
             }
+          }
+          Err(err) => {
+            warn!("{}", err);
+          }
         }
         info!("Redis is not ready, sleeping for 1 second");
         std::thread::sleep(Duration::from_secs(1));
