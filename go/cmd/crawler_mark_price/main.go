0ae0104a422857db60c71479420b73330c616870
soulmachine <soulmachine@gmail.com>
2021-10-19 10:28:32 +0000

    Get price from carbonbot:funding_rate instead of websocket

--- go/cmd/crawler_mark_price/main.go
+++ go/cmd/crawler_mark_price/main.go
@@ -6,8 +6,9 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
-	"github.com/gorilla/websocket"
+	"github.com/buger/jsonparser"
 	"github.com/soulmachine/coinsignal/config"
 	"github.com/soulmachine/coinsignal/pojo"
 	"github.com/soulmachine/coinsignal/pubsub"
@@ -27,22 +28,39 @@ func main() {
 		log.Fatal("The REDIS_URL environment variable is empty")
 	}
 	utils.WaitRedis(ctx, redis_url)
+
+	rdb := utils.NewRedisClient(redis_url)
 	publisher := pubsub.NewPublisher(ctx, redis_url)
 
-	// https://binance-docs.github.io/apidocs/futures/en/#mark-price-stream
-	client, _, err := websocket.DefaultDialer.Dial("wss://fstream.binance.com/ws/!markPrice@arr", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		_, bytes, _ := client.ReadMessage()
+	pubsub := rdb.Subscribe(ctx,
+		config.REDIS_TOPIC_FUNDING_RATE,
+	)
+
+	// Consume messages.
+	for msg := range pubsub.Channel() {
+		raw_msg := pojo.CarbonbotMessage{}
+		json.Unmarshal([]byte(msg.Payload), &raw_msg)
+		if raw_msg.Exchange != "binance" {
+			continue
+		}
+
+		raw_json := []byte(raw_msg.Json)
+		data, _, _, _ := jsonparser.Get(raw_json, "data")
+
 		var mark_prices_raw []MarkPriceRaw
-		if err := json.Unmarshal(bytes, &mark_prices_raw); err != nil {
+		if err := json.Unmarshal(data, &mark_prices_raw); err != nil {
 			panic(err)
 		}
 
 		for _, mark_price_raw := range mark_prices_raw {
-			currency := mark_price_raw.Symbol[:len(mark_price_raw.Symbol)-4]
+			var currency string
+			if strings.HasSuffix(mark_price_raw.Symbol, "USD_PERP") {
+				currency = mark_price_raw.Symbol[:len(mark_price_raw.Symbol)-8]
+			} else if strings.HasSuffix(mark_price_raw.Symbol, "USDT") || strings.HasSuffix(mark_price_raw.Symbol, "BUSD") {
+				currency = mark_price_raw.Symbol[:len(mark_price_raw.Symbol)-4]
+			} else {
+				continue
+			}
 			price, _ := strconv.ParseFloat(mark_price_raw.Price, 64)
 
 			currency_price := pojo.CurrencyPrice{
@@ -54,5 +72,7 @@ func main() {
 			publisher.Publish(config.REDIS_TOPIC_CURRENCY_PRICE_CHANNEL, string(json_bytes))
 		}
 	}
-	// publisher.Close()
+
+	pubsub.Close()
+	publisher.Close()
 }
