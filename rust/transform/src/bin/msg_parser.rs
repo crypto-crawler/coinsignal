ce0187972b1fea0414f3fd886c3d3688e36b2837
soulmachine <soulmachine@gmail.com>
2021-10-21 11:43:56 +0000

    Added 4 parser threads

--- rust/transform/src/bin/msg_parser.rs
+++ rust/transform/src/bin/msg_parser.rs
@@ -1,4 +1,7 @@
-use std::{sync::mpsc::Receiver, thread::JoinHandle};
+use std::{
+    sync::mpsc::{Receiver, Sender},
+    thread::JoinHandle,
+};
 
 use crypto_msg_parser::{FundingRateMsg, MarketType, MessageType, TradeMsg};
 use log::*;
@@ -8,6 +11,7 @@ use utils::{pubsub::Publisher, wait_redis};
 
 const REDIS_TOPIC_TRADE: &str = "carbonbot:trade";
 const REDIS_TOPIC_FUNDING_RATE: &str = "carbonbot:funding_rate";
+const NUM_PARSER_THREADS: usize = 4;
 
 /// Message represents messages received by crawlers.
 #[derive(Serialize, Deserialize)]
@@ -24,9 +28,13 @@ pub struct Message {
     pub json: String,
 }
 
-fn create_parser_thread(rx: Receiver<Message>, redis_url: String) -> JoinHandle<()> {
+fn create_parser_thread(
+    thread_name: String,
+    rx: Receiver<Message>,
+    redis_url: String,
+) -> JoinHandle<()> {
     std::thread::Builder::new()
-        .name("parser".to_string())
+        .name(thread_name)
         .spawn(move || {
             let mut publisher = Publisher::new(&redis_url);
             for raw_msg in rx {
@@ -94,14 +102,21 @@ fn main() {
     pubsub.subscribe(REDIS_TOPIC_TRADE).unwrap();
     pubsub.subscribe(REDIS_TOPIC_FUNDING_RATE).unwrap();
 
-    let (tx, rx) = std::sync::mpsc::channel::<Message>();
-    let _ = create_parser_thread(rx, redis_url.to_string());
+    let senders = (0..NUM_PARSER_THREADS)
+        .map(|i| {
+            let (tx, rx) = std::sync::mpsc::channel::<Message>();
+            let _ = create_parser_thread(format!("parser-{}", i), rx, redis_url.to_string());
+            tx
+        })
+        .collect::<Vec<Sender<Message>>>();
+    let mut index: usize = 0;
     loop {
         match pubsub.get_message() {
             Ok(msg) => {
                 let payload: String = msg.get_payload().unwrap();
                 let raw_msg = serde_json::from_str::<Message>(&payload).unwrap();
-                tx.send(raw_msg).unwrap();
+                senders[index].send(raw_msg).unwrap();
+                index = (index + 1) % NUM_PARSER_THREADS;
             }
             Err(err) => error!("line 106 {}", err),
         }
